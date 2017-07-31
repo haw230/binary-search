68c786b0189b4796755c50c07508f463e32d2916
hanchengwu <hancheng.wu22@gmail.com>
2017-07-31 16:46:37 +0000

    added some stuff

--- solution/solution.py
+++ solution/solution.py
@@ -1,10 +1,10 @@
 def solved_binary_check(ls, num):
-    if(low > high):
-        return -1
-    mid = int((low + high) / 2)
-    if(ls[mid] == num):
-        return mid
-    elif(num > ls[mid]):
-        return solved_binary_search(ls, num, mid + 1, high)
-    elif(num < ls[mid]):
-        return solved_binary_search(ls, num, low, mid - 1)
+    mid = int(len(ls) / 2) #middle of list
+    if(ls[mid] == num): #if middle is the number
+        return True #then it's in the list
+    if(mid <= 1): #if the list has one element and it's not num
+        return False #it's not in the list
+    if(ls[mid] > num): #if list at middle is greater than num
+        return solved_binary_check(ls[mid + 1:], num) #search 
+    if(ls[mid] < num):
+        return solved_binary_check(ls[:mid - 1], num)
